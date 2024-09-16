6e09e1b4dd41ddbd596c76725b119667d29f3910
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2024-09-16 01:05:46 +0100

    feat: index page && header component

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  darkMode: "selector",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        themeOne: "#01251d",
+        themeTwo: "#072b35",
+        themeThree: "#025744",
+        themeFour: "#14aea1",
+        themeFive: "#03896B",
+        themeSix: "#011712",
+      },
+    },
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 };
